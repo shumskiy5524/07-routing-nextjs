0d4766bf0ea87ba3dc492b93f1e024177827dc5b
shumskiy <shumskiparents@gmail.com>
2026-04-06 14:27:56 +0200

    fix code and imports

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,8 @@
 
-import css from './NotFound.module.css';
-
 export default function NotFound() {
   return (
-    <div className={css.wrapper}>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '2rem' }}>
+      Page Not Found
     </div>
   );
 }
